24fe4b0676c643be14624c3d1078b92343ecdc22
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2019-02-04 14:31:58 +0100

    Allows running graphviz examples from any location
    Examples can now be run from any directory. For example, to run them
    from the root of the checkout run ./examples/graphviz/all.py

--- examples/graphviz/all.py
+++ examples/graphviz/all.py
@@ -3,10 +3,12 @@
 Execute all pycallgraph examples in this directory.
 '''
 from glob import glob
+import os
 
 
-examples = glob('*.py')
-examples.remove('all.py')
+curdir = os.path.dirname(os.path.abspath(__file__))
+examples = glob(os.path.join(curdir, '*.py'))
+examples.remove(os.path.join(curdir,'all.py'))
 for example in examples:
     print(example)
     execfile(example)
